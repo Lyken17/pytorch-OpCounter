Fix calculation formula for batch norm.

thop/count_hooks.py
@@ -45,8 +45,9 @@ def count_bn(m, x, y):
     x = x[0]
 
     nelements = x.numel()
-    # subtract, divide, gamma, beta
-    total_ops = 4 * nelements
+    if not m.training:
+        # subtract, divide, gamma, beta
+        total_ops = 2 * nelements
 
     m.total_ops += torch.Tensor([int(total_ops)])
 
@@ -73,9 +74,10 @@ def count_softmax(m, x, y):
 
 
 def count_avgpool(m, x, y):
-    total_add = torch.prod(torch.Tensor([m.kernel_size]))
-    total_div = 1
-    kernel_ops = total_add + total_div
+    # total_add = torch.prod(torch.Tensor([m.kernel_size]))
+    # total_div = 1
+    # kernel_ops = total_add + total_div
+    kernel_ops = 1
     num_elements = y.numel()
     total_ops = kernel_ops * num_elements
 

thop/profile.py
@@ -64,7 +64,7 @@ def profile(model, inputs, custom_ops=None, verbose=True):
             return
 
         if hasattr(m, "total_ops") or hasattr(m, "total_params"):
-            logger.warning("Either .total_ops or .total_params is already defined in %s."
+            logger.warning("Either .total_ops or .total_params is already defined in %s. "
                            "Be careful, it might change your code's behavior." % str(m))
 
         m.register_buffer('total_ops', torch.zeros(1))
@@ -82,10 +82,10 @@ def profile(model, inputs, custom_ops=None, verbose=True):
 
         if fn is None:
             if verbose:
-                print("THOP has not implemented counting method for ", m)
+                logger.info("THOP has not implemented counting method for ", m)
         else:
             if verbose:
-                print("Register FLOP counter for module %s" % str(m))
+                logger.info("Register FLOP counter for module %s" % str(m))
             handler = m.register_forward_hook(fn)
             handler_collection.append(handler)
 
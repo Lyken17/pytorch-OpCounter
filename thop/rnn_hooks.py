cac68fd9ab711562c61aef73ba4f3d83121ac03d
BetaCat <60107867+timmywanttolearn@users.noreply.github.com>
2021-09-10 16:08:57 +0800

    adding hook function for nn.Transformer
    not available for custom_encoder and custom_decoder

--- thop/rnn_hooks.py
+++ thop/rnn_hooks.py
@@ -196,3 +196,86 @@ def count_lstm(m: nn.LSTM, x, y):
     total_ops *= batch_size
 
     m.total_ops += torch.DoubleTensor([int(total_ops)])
+    
+def count_Transformer(m: nn.Transformer, x, y):
+    total_ops = 0
+    src, tgt = x
+    if m.batch_first:
+        num_steps = src.shape[0]
+        target = tgt.shape[1]
+        sequence = src.shape[1]
+        embedding = src.shape[2]
+    else:
+        target = tgt.shape[0]
+        sequence = src.shape[0]
+        num_steps = src.shape[1]
+        embedding = src.shape[2]
+    num_head = m.nhead
+    encoder_layers = m.encoder.num_layers
+    decoder_layers = m.decoder.num_layers
+    # dim_forward(default = 2048)
+    forward = m.encoder.layers[0].linear1.out_features
+    total_ops = 0
+
+    def MultiheadAttention(bool1, num_head, num_steps, target, sequence, embedding):
+        if bool1 == 0:
+            # linear_q,linear_k,linear_v all N,S,E
+            total_multi = 3 * sequence * embedding ** 2
+            # self_attn softmax(Q*K_T/sqrt(dk))*V
+            total_multi += sequence ** 4 * embedding ** 2 + \
+                sequence ** 2 + sequence * (3 * sequence - 1) + 1
+        elif bool1 == 1:
+            # linear_q,linear_k,linear_v
+            total_multi = 3 * target * embedding ** 2
+            # self_attn softmax(Q*K_T/sqrt(dk))*V
+            total_multi += target ** 4 * embedding ** 2 + \
+                target ** 2 + target * (3 * target-1) + 1
+        elif bool1 == 2:
+            # linear_q,linear_k,linear_v
+            total_multi = embedding ** 2 * (2 * sequence + target)
+            # self_attn softmax(Q*K_T/sqrt(dk))*V
+            total_multi += target * (sequence ** 2) * embedding ** 3 + \
+                target * sequence + target * (3 * sequence - 1)+1
+        # number of heads and batchsize
+        total_multi *= total_multi * num_head*num_steps
+        # print(total_multi)
+        # concat
+        if bool1 == 0:
+            total_multi += num_steps * (sequence ** 2 * num_head * embedding)
+            # print(total_multi)
+        else:
+            total_multi += num_steps * (target ** 2 * num_head * embedding)
+        # output-> (N,S,E) or (S,N,E)
+        return total_multi
+
+    def TransformerEncoderLayer(num_head, num_steps, target, sequence, embedding):
+        total_en = 0
+        total_en += MultiheadAttention(0, num_head,
+                                       num_steps, target, sequence, embedding)
+        # linear1 in_features= embedding, outfeatures= dim_forward
+        total_en += num_steps * sequence * forward * (embedding ** 2)
+        # linear2
+        total_en += num_steps * sequence * embedding * (forward ** 2)
+        # norm1 norm2
+        total_en += 2 * 2 * num_steps * embedding * sequence
+        # droup out 2,3
+        return total_en
+
+    def TransformerDecoderLayer(num_head, num_steps, target, sequence, embedding):
+        total_de = 0
+        total_de += MultiheadAttention(1, num_head,
+                                       num_steps, target, sequence, embedding)
+        total_de += MultiheadAttention(2, num_head,
+                                       num_steps, target, sequence, embedding)
+        # linear1 linear2 fft
+        total_de += num_steps * target * forward * (embedding ** 2)
+        total_de += num_steps * target * embedding * (forward ** 2)
+        # 3* norm
+        total_de += 3 * 2 * num_steps * embedding * target
+        return total_de
+    total_ops = encoder_layers * TransformerEncoderLayer(num_head, num_steps, target, sequence, embedding) + \
+        decoder_layers * \
+        TransformerDecoderLayer(num_head, num_steps,
+                                target, sequence, embedding)
+    m.total_ops += torch.DoubleTensor([int(total_ops)])
+

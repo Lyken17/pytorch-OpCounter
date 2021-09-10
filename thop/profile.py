b25be5eec18667f6634742a68fb73f4a3c9a7105
BetaCat <60107867+timmywanttolearn@users.noreply.github.com>
2021-09-10 15:06:17 +0800

    Add hook for nn.Transformer

--- thop/profile.py
+++ thop/profile.py
@@ -67,6 +67,7 @@ register_hooks = {
     nn.RNN: count_rnn,
     nn.GRU: count_gru,
     nn.LSTM: count_lstm,
+    nn.Transformer: count_Transformer
 }
 
 if LooseVersion(torch.__version__) >= LooseVersion("1.1.0"):

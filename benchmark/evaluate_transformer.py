fd9f2ecea157f317312b37be8a35a3e28e59a631
BetaCat <60107867+timmywanttolearn@users.noreply.github.com>
2021-09-10 16:05:31 +0800

    add a example to test transformer
    The answer is 2.76945964418343e+25 44151808.0.

---
+++ benchmark/evaluate_transformer.py
@@ -0,0 +1,23 @@
+
+import torch.nn as nn
+from thop import profile
+import torch
+layers = []
+layers.append(nn.Transformer())
+
+src=torch.rand((10, 32, 10))
+class model1(nn.Module):
+    def __init__(self):
+        super(model1, self).__init__()
+        self.linear1 = nn.Linear(10,512)
+        self.linear2 = nn.Linear(10,512)
+        self.transform = nn.Transformer()
+    def forward(self,input):
+        input1 = self.linear1(input)
+        input2 = self.linear2(input)
+        output = self.transform(input1,input2)
+        return output
+model2 = nn.Sequential(model1())
+macs, params = profile(model2, inputs=(src, ))
+print(macs,params)
+

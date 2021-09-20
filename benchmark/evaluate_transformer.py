f6fa0b03719fb5364b0ca1e947c80042483d7188
BetaCat <60107867+timmywanttolearn@users.noreply.github.com>
2021-09-20 11:37:03 +0800

    change about examples
    For one word, about 25G flops and 44M parameters

--- benchmark/evaluate_transformer.py
+++ benchmark/evaluate_transformer.py
@@ -2,22 +2,20 @@
 import torch.nn as nn
 from thop import profile
 import torch
-layers = []
-layers.append(nn.Transformer())
 
-src=torch.rand((10, 32, 10))
-class model1(nn.Module):
+src=torch.rand((1, 1, 10))# S,N,x
+class Model_transformer(nn.Module):
     def __init__(self):
-        super(model1, self).__init__()
+        super(Model_transformer, self).__init__()
         self.linear1 = nn.Linear(10,512)
         self.linear2 = nn.Linear(10,512)
-        self.transform = nn.Transformer()
+        self.transform = nn.Transformer(d_model = 512 ,nhead = 8, num_encoder_layers = 6)
     def forward(self,input):
         input1 = self.linear1(input)
         input2 = self.linear2(input)
         output = self.transform(input1,input2)
         return output
-model2 = nn.Sequential(model1())
-macs, params = profile(model2, inputs=(src, ))
+model = Model_transformer()
+macs, params = profile(model, inputs=(src, ))
 print(macs,params)
 

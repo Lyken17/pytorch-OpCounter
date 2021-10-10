cfa6f8d4630e5b096704d98a09d0758a0e9c6964
Cat Beta <catbeta@CatdeMacBook-Air.local>
2021-10-10 20:59:14 +0800

    jit finished

--- test_jit.py
+++ test_jit.py
@@ -1,23 +1,6 @@
 import torch
-from torchvision.models import resnet18,vgg11
-from torchprofile import profile_macs
-import torch.nn as nn
+from torchvision.models import vgg11
 from thop import JitProfile
-input = torch.rand(1,10)
-class model1(nn.Module):
-    def __init__(self):
-        super(model1, self).__init__()
-        self.linear1 = nn.Linear(10, 20)
-        self.linear2 = nn.Linear(20,10)
-    def forward(self,input1):
-
-        lin1 = self.linear1(input1)
-        lin2 = self.linear2(lin1)
-        return lin2
-linear = model1()
-linear_jit = torch.jit.script(linear)
-linear_trace = torch.jit.trace(linear,input)
-#print(JitProfile.calculate_params(linear))
 model1 = vgg11()
 input1 = torch.rand(1,3,224,224)
 print(JitProfile.calculate_macs(model1,input1))

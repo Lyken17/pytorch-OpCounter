e882f88fe9bfc12a1ec3a48c7e94b673f574a8dc
naviak <m.dudar@g.nsu.ru>
2022-02-16 12:25:05 +0300

    kwargs

--- thop/profile.py
+++ thop/profile.py
@@ -155,7 +155,7 @@ def profile_origin(model, inputs, custom_ops=None, verbose=True, report_missing=
 
 def profile(
     model: nn.Module,
-    inputs,
+    kwargs,
     custom_ops=None,
     verbose=True,
     ret_layer_info=False,
@@ -208,7 +208,7 @@ def profile(
     model.apply(add_hooks)
 
     with torch.no_grad():
-        model(*inputs)
+        model(**kwargs,)
 
     def dfs_count(module: nn.Module, prefix="\t") -> (int, int):
         total_ops, total_params = module.total_ops.item(), 0
